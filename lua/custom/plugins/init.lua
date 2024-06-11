1c98bfd26671ea3969c8815e73efd65929dc4ca0
emigpa <emigpa@gmail.com>
2024-06-11 20:49:53 -0300

    show file name to window frame

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,5 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-
+vim.opt.title = true
+vim.opt.titlestring = '%F'
 return {}
